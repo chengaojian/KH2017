fb013227d3bd5528c114f636d04f8ea66146d9ad
chengaojian <chengaojian@haixun360.com>
2017-06-10 10:01:42 +0800

    创建项目

---
+++ KH2017/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KH2017
+//
+//  Created by 三海教育 on 2017/6/10.
+//  Copyright © 2017年 三海教育. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KH2017/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  KH2017
+//
+//  Created by 三海教育 on 2017/6/10.
+//  Copyright © 2017年 三海教育. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
